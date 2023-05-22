994be50c20a179b14a4841e10031a9abc0e849bf
Ryan <99357724+Itsme104@users.noreply.github.com>
2023-05-22 14:55:33 -0400

    Create Dialogue.py

---
+++ Dialogue.py
@@ -0,0 +1,36 @@
+# Name: Ryan Stone
+# Date: 5/19/2023
+# Project: Dialouge
+
+from time import sleep
+from os import system
+
+def gameStart():
+  sleep(2)
+  print("Dilly Land is a beautiful small town surrounded by corn fields and mountains.")
+  print("You live in Dilly Land on a farm house.")
+  sleep(5)
+  print("Recently you have been craving pickles.")
+  sleep(4)
+  system('clear')
+  print("You suddenly wake up in the middle of the night.")
+  sleep(1.5)
+  print("You NEED pickles. No not just pickles.. pickle juice.")
+  sleep(3)
+  system('clear')
+
+def buck():
+  print("You see a lot of points in the darkness that surrounds you.\n")
+  sleep(3)
+
+def conway():
+  print("In the opening of the field you see a farmer?")
+  sleep(2)
+  print("No its a scarecrow..")
+  sleep(1)
+  print("But why is it moving...\n")
+  sleep(3)
+
+def rick():
+  print("Is that... PICKLE RICK!?\n")
+  sleep(3)
